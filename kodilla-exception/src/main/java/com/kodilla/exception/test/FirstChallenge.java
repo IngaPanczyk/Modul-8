9b3acaca43c4c517536d63fd52d2a2672e9c78aa
IngaPanczyk <ingawaaa@gmail.com>
2020-06-23 22:01:16 +0200

    Zadanie 1.0

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,32 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        try {
+            if (b == 0) {
+                throw new ArithmeticException();
+            }
+        } catch (ArithmeticException arithmeticException) {
+            System.out.println("Nie dziel przez 0");
+        } finally {
+            System.out.println("Obliczenia zakończone!");
+        }
+        return a / b;
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        double result = firstChallenge.divide(3, 0);
+
+        System.out.println(result);
+
+    }
+}
