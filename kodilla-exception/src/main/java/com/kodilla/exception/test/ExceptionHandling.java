eafabb7d0b5ab4b08800d6c0623231370cb51112
IngaPanczyk <ingawaaa@gmail.com>
2020-06-24 20:39:10 +0200

    Zadanie 2.1

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -5,9 +5,11 @@ public class ExceptionHandling extends Throwable {
     public static void main(String[] args) {
         SecondChallenge secondChallenge = new SecondChallenge();
         try {
-            secondChallenge.probablyIWillThrowException(1, 1);
-        } catch (ExceptionHandling e) {
+            secondChallenge.probablyIWillThrowException(1, 1.5);
+        } catch (Exception e) {
             System.out.println("Exception!!!");
+        } finally {
+            System.out.println("Program zakończony!");
         }
     }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/SecondChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/SecondChallenge.java
@@ -1,20 +1,10 @@
 package com.kodilla.exception.test;
 
-import java.sql.SQLOutput;
-
 public class SecondChallenge {
-    public String probablyIWillThrowException(double x, double y) throws ExceptionHandling {
-        try {
-            if (x >= 2 || x < 1 || y == 1.5) {
-                throw new Exception();
-            }
-            System.out.println("DONE");
-            return "Done!";
-        } catch (Exception e) {
-            throw new ExceptionHandling();
-        } finally {
-            System.out.println("Program completed");
-
+    public String probablyIWillThrowException(double x, double y) throws Exception {
+        if(x >= 2 || x < 1 || y == 1.5) {
+            throw new Exception();
         }
+        return "Done!";
     }
 }
